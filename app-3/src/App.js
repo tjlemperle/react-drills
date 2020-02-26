01876a24766d8f0e5234a7dffb62df78c901d0d5
tjlemperle <tjlemperle@gmail.com>
2020-02-26 15:15:07 -0700

    third drill

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      input: '',
+
+      characters: ['Jim', 'Pam', 'Dwight', 'Michael', 'Andy', 'Angela']
+    }
+  }
+
+  handleChange(value){
+    this.setState({input: value})
+  }
+
+  render(){
+    let charToDisplay = this.state.characters
+      .filter((element, indext) => {
+        return element.toLowerCase().includes(this.state.input);
+      })
+      .map((element, index) => {
+        return <h2 key={index}>{element}</h2>}
+      );
+
+    return (
+      <div className="App">
+        <input onChange={e => this.handleChange(e.target.value)} type='text' />
+        {charToDisplay}
+      </div>
+    );
+  }
 }
 
 export default App;
